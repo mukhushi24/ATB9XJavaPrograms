fb9a55a14fa89048e55d383c5ab20e54083c0512
Khushi M U <mukhushi24@gmail.com>
2024-10-09 21:37:06 +0530

    File

--- src/Task/Task_0302024Ten/Lab__001.java
+++ src/Task/Task_0302024Ten/Lab__001.java
@@ -3,16 +3,17 @@ package Task.Task_0302024Ten;
 public class Lab__001 {
     public static void main(String[] args) {
 
+        System.out.println("Hello");
         System.out.println("10*1=10");
-        System.out.println("10*1=20");
-        System.out.println("10*1=30");
-        System.out.println("10*1=40");
-        System.out.println("10*1=50");
-        System.out.println("10*1=60");
-        System.out.println("10*1=70");
-        System.out.println("10*1=80");
-        System.out.println("10*1=90");
-        System.out.println("10*1=100");
+        System.out.println("10*2=20");
+        System.out.println("10*3=30");
+        System.out.println("10*4=40");
+        System.out.println("10*5=50");
+        System.out.println("10*6=60");
+        System.out.println("10*7=70");
+        System.out.println("10*8=80");
+        System.out.println("10*9=90");
+        System.out.println("10*10=100");
 
     }
 }
