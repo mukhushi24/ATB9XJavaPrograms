72c25f6a32b00192911c8106728966e9b84884a7
Khushi M U <mukhushi24@gmail.com>
2024-11-05 22:57:31 +0530

    files

--- src/Task/Task__05112024/Lab_52.java
+++ src/Task/Task__05112024/Lab_52.java
@@ -70,4 +70,22 @@ public class Lab_52 {
 
 
     }
+    static class Dog_5 {
+        String name;
+        int age;
+        String breed;
+        double weight;
+
+        void bark(String pitch) {
+            System.out.println(name + "  barks with " + pitch + " pitch");
+        }
+
+        void eat() {
+            System.out.println(name + " is eating.");
+        }
+
+        void play(int balls) {
+            System.out.println(name + " plays with " + balls + " balls");
+        }
+    }
 }
